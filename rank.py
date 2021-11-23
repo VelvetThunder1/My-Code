57c22fa03d02393649fb54067fe43603f8a21827
VelvetThunder1 <74422927+VelvetThunder1@users.noreply.github.com>
2021-11-23 16:34:28 +0530

    Delete rank.py

--- rank.py
+++
@@ -1,7 +0,0 @@
-import sys
-from itertools import permutations
-word = sys.argv[1]
-ini_str = word
-permutation = [''.join(p) for p in permutations(word)]
-permutation.sort()
-print(int(permutation.index(word))+1)
